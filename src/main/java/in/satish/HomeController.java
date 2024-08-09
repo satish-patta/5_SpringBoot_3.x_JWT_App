793572407de451b818720599aa2cf8612645f35b
ihavepositiveattitude <kartikoleti@123>
2024-08-09 07:22:25 +0530

    spring boot 3.x

---
+++ src/main/java/in/satish/HomeController.java
@@ -0,0 +1,18 @@
+package in.satish;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @GetMapping("/hi")
+    public String test() {
+        this.logger.warn("method is called...");
+        return "Welcome to Battle Field..!!";
+    }
+}
---
+++ src/main/java/in/satish/SbmsJwtSecurityAppApplication.java
@@ -0,0 +1,13 @@
+package in.satish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmsJwtSecurityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmsJwtSecurityAppApplication.class, args);
+	}
+
+}
